5a51fc882071cd2a2e9abe0e71a33b1cda582300
BlackAssassinCoder <Ahmadasadi1381.22@gmail.com>
2024-04-04 15:53:25 +0330

    add password visibility

---
+++ public/JS/FrontEnd/Visibility/passwordVisibility.js
@@ -0,0 +1,14 @@
+function togglePasswordVisibility(pass, ico) {
+    var passwordField = document.querySelector(pass);
+    var icon = document.getElementById(ico);
+
+    if (passwordField.type === 'password') {
+        passwordField.type = 'text';
+        icon.classList.remove('fa-eye');
+        icon.classList.add('fa-eye-slash');
+    } else {
+        passwordField.type = 'password';
+        icon.classList.remove('fa-eye-slash');
+        icon.classList.add('fa-eye');
+    }
+}
