e63b456df1394317dc6b10d06b17b0e0ab2769c7
BlackAssassinCoder <Ahmadasadi1381.22@gmail.com>
2024-04-04 10:08:49 +0330

    add main file

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const port = 5656;
+
+const routeMain = require(path.join(__dirname, 'public','JS', 'BackEnd', 'Route', 'MainRoute.js'));
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', routeMain);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
