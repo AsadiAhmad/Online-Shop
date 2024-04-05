a4dff4c7cf16bede061191796a07ace15ee4f2df
BlackAssassinCoder <Ahmadasadi1381.22@gmail.com>
2024-04-06 03:29:11 +0330

    create product page route

--- public/JS/BackEnd/API/ProductAPI.js
+++ public/JS/BackEnd/API/ProductAPI.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const connection = require(path.join(__dirname, '..', 'Connection', 'connection.js'));
 const ejs = require("ejs");
 const ProductAPI = (req, res) => {
     res.sendFile(path.join(__dirname, '..', '..', '..', 'HTML', 'Product', 'product.html'));
@@ -8,9 +9,15 @@ const ProductPageAPI = async (req, res) => {
     try {
         const db = await connection();
         const productsCollection = db.collection('Products');
-        const products = await productsCollection.find().toArray();
+        const productId = req.params.productId; // Get the product ID from the URL
+        const product = await productsCollection.findOne({ _id: productId }); // Fetch the specific product
 
-        ejs.renderFile(path.join(__dirname, '..', '..', '..', 'EJS', 'product.ejs'), { products: products }, (err, html) => {
+        if (!product) {
+            res.status(404).send('Product not found');
+            return;
+        }
+
+        ejs.renderFile(path.join(__dirname, '..', '..', '..', 'EJS', 'productDetail.ejs'), { product: product }, (err, html) => {
             if (err) {
                 console.error("Failed to render EJS file:", err);
                 res.status(500).send('Error rendering EJS file');
@@ -19,8 +26,8 @@ const ProductPageAPI = async (req, res) => {
             }
         });
     } catch (e) {
-        console.error("Failed to load products:", e);
-        res.status(500).send('Error loading products');
+        console.error("Failed to load product:", e);
+        res.status(500).send('Error loading product');
     }
 };
 
