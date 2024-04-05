2ac0b0bbc262ce9c2d42a28ca43f1841d1037b79
BlackAssassinCoder <Ahmadasadi1381.22@gmail.com>
2024-04-05 12:22:10 +0330

    add login route

--- public/JS/BackEnd/Route/LoggingRoute.js
+++ public/JS/BackEnd/Route/LoggingRoute.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require("path");
 const router = express.Router();
 
-// const loginAPI = require(path.join(__dirname, '..', 'Login', 'login.js'));
+const loginAPI = require(path.join(__dirname, '..', 'Login', 'login.js'));
 const signupAPI = require(path.join(__dirname, '..', 'Login', 'signUp.js'));
 
-// router.put('/login', loginAPI);
+router.put('/login', loginAPI);
 router.post('/signup', signupAPI);
 
 module.exports = router;
